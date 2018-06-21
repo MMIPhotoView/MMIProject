64db74c95ff3a957c06e308d28f8fc2d88900b61
151250068 <151250068@smail.nju.edu.cn>
2018-06-21 13:54:38 +0800

    注册界面

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -11,8 +11,36 @@ class Home extends React.Component {
   render() {
     return (
       <div className='container'>
-        <div>注册</div>
+          <div className="login_m">
+            <div className="login_boder" >
+              <div className="login_padding">
+                <h2>用户名</h2>
+                <label style={{height:'5px'}}>
+                  <input type="text" id="username" className="txt_input txt_input2" placeholder = 'Your Username'
+                           value = ""/>
+                </label>
+                <h2>密码</h2>
+                <label style={{height:'5px'}}>
+                <input type="password" name="textfield2" id="userpwd" className="txt_input" placeholder = 'Your Password'
+                         value = ""/>
+                </label>
+                <h2>再次输入</h2>
+                <label style={{height:'5px'}}>
+                  <input type="password" name="textfield2" id="userpwd" className="txt_input" placeholder = 'Your Password'
+                         value = ""/>
+                </label>
+                <p></p>
+                <div className="rem_sub">
+
+                  <label   style={{float:'right', marginRight:'10px', marginTop:'10px'}}>
+                    <button className="sub_button">注册</button>
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
       </div>
+
     );
   }
 }
