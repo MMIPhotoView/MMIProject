92071c48be6f174617eed44df71064e74efc290c
151250068 <151250068@smail.nju.edu.cn>
2018-06-30 12:14:11 +0800

    关注界面

--- src/components/FollowList/Item/index.jsx
+++ src/components/FollowList/Item/index.jsx
@@ -11,14 +11,31 @@ class FollowItem extends React.Component {
   }
   render() {
     return (
-      <div>
-        <button style={{height:'30px'}} onClick={this.test} >sdsdsdsdsdsdsds</button>
+      <div className='boder'>
+
+        <div className='boder selection'>
+          <div className='boder img_wrap'>
+            <img alt="更改头像" className='img_icon' src=""/>
+          </div>
+          <div className='boder ifo_wrap'>
+            <div className='ifo_1'>aaaaaaaaa</div>
+            <div className='ifo_2'>bbbbsadsafsafsabbbbbbbb</div>
+          </div>
+          <div className='boder button'>
+            <button style={{width:'80%'}} onClick={this.test} >关注</button>
+          </div>
+        </div>
+
+
+        <hr />
+
       </div>
     );
   }
 
   test() {
     alert('123')
+    //alert(document.getElementsByClassName('boder button').innerHTML);
   }
 
 }
