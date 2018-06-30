75cb0dc9606486bb24a711dee50c3a8d93e583ef
gaoxiang15125 <630268696@qq.com>
2018-06-30 16:49:51 +0800

    中间提交
    提交中间代码

--- src/components/EditUserInfo/index.jsx
+++ src/components/EditUserInfo/index.jsx
@@ -12,10 +12,28 @@ class EditUserInfo extends React.Component {
   render() {
     return (
       <div>
-        <div>编辑界面</div>
+
+        <div id={'outAll'}>
+          <div id={'outAll1'}>
+            <div id={'clipArea'}></div>
+            <div id={'outAll3'}>
+              <button id="clipBtn">上传头像</button>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div id={'view'} title={'请上传 428*321 的图片'}></div>
+          <div id={'blank'}></div>
+          <div id={'outAll4'}>
+            编辑头像
+          </div>
+        </div>
       </div>
     );
   }
+
+
 }
 
 
