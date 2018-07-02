8be2caa825923657acce2dc3b12d1ab0888db4a5
151250068 <151250068@smail.nju.edu.cn>
2018-07-02 17:48:58 +0800

    添加用户的主页跳转和注销

--- src/components/MainNav/index.jsx
+++ src/components/MainNav/index.jsx
@@ -3,19 +3,24 @@ import { NavLink } from 'react-router-dom'
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import './style.less'
 // import SearchComponent from '../Search'
-import { Menu, Affix } from 'antd';
+import { Menu, Affix, Avatar, Icon } from 'antd';
 import Upload from '../../components/Upload'
 
-
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
 class Nav extends React.Component {
 
     constructor(props, context) {
         super(props,context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+        this.state = {
+          current: 'mail',
+        }
+
 
     }
-
     render() {
+
         return (
             <div>
             <Affix offsetTop={0}>
@@ -25,10 +30,14 @@ class Nav extends React.Component {
                     </Menu.Item>
 
 
+                    <SubMenu style={{float:'right'}} title={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}>
+                        <Menu.Item key="setting:1"><Icon type="user" />用户主页</Menu.Item>
+                        <Menu.Item key="setting:2"><Icon type="logout" />注销</Menu.Item>
 
+                    </SubMenu>
 
                     <Menu.Item key="user" style={{float:'right'}}>
-                        <NavLink to={'/Login'}>用户</NavLink>
+                        <NavLink to={'/Login'}>登录</NavLink>
                     </Menu.Item>
 
                     <Menu.Item key="upload" style={{float:'right'}} >
